6ce6b7f4e33946caef93503879232626b2649c5a
icy <kyanh@viettug.org>
2012-07-20 10:04:54 +0700

    change: fix format

--- content/changes.rb
+++ content/changes.rb
@@ -6,7 +6,7 @@ title: Thay đổi gần đây
 
 <p>
   Dưới đây là những thay đổi gần đây của trang chủ
-  _(trên nhánh `master`)_.
+  <em>(trên nhánh <code>master</code>)</em>.
   Các liên kết dẫn tới kho ở Github. Vì kho ở Github
   không được cập nhật thường xuyên, các liên kết
   mới nhất có thể không hợp lệ. Bạn có thể chờ đến
