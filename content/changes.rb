fdf372f380b6763724a900005b47f543debd67f9
icy <kyanh@viettug.org>
2012-07-20 10:03:49 +0700

    changes: add note

--- content/changes.rb
+++ content/changes.rb
@@ -5,11 +5,12 @@ title: Thay đổi gần đây
 <h1>Thay đổi gần đây</h1>
 
 <p>
-  Dưới đây là những thay đổi gần đây của trang chủ.
+  Dưới đây là những thay đổi gần đây của trang chủ
+  _(trên nhánh `master`)_.
   Các liên kết dẫn tới kho ở Github. Vì kho ở Github
   không được cập nhật thường xuyên, các liên kết
   mới nhất có thể không hợp lệ. Bạn có thể chờ đến
-  ngày hôm sau thì mọi chuyện sẽ bình thường
+  ngày hôm sau thì mọi chuyện sẽ bình thường.
 </p>
 
 <%= recent_changes %>
