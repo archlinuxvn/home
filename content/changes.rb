4d4fde2cf24a16ecb0ffa5e9162f0c7c05fa4753
icy <kyanh@viettug.org>
2012-07-21 15:12:02 +0700

    changes/Rules: multiple filter support

--- content/changes.rb
+++ content/changes.rb
@@ -2,15 +2,11 @@
 title: Thay đổi gần đây
 ---
 
-<h1>Thay đổi gần đây</h1>
+# Thay đổi gần đây
 
-<p>
-  Dưới đây là những thay đổi gần đây của trang chủ
-  <em>(trên nhánh <code>master</code>)</em>.
-  Các liên kết dẫn tới kho ở Github. Vì kho ở Github
-  không được cập nhật thường xuyên, các liên kết
-  mới nhất có thể không hợp lệ. Bạn có thể chờ đến
-  ngày hôm sau thì mọi chuyện sẽ bình thường.
-</p>
+Dưới đây là những thay đổi gần đây của trang chủ _(trên nhánh `master`)_.
+Các liên kết dẫn tới kho ở Github. Vì kho ở Github không được cập nhật
+thường xuyên, các liên kết mới nhất có thể không hợp lệ. Bạn có thể chờ đến
+ngày hôm sau thì mọi chuyện sẽ bình thường.
 
 <%= recent_changes %>
