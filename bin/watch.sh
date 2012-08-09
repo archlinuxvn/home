6cd5dc8a99231e27ccc69e9e3e7aa805491015db
icy <kyanh@viettug.org>
2012-08-09 21:39:15 +0700

    bin/watch: support two action view, watch

--- bin/watch.sh
+++ bin/watch.sh
@@ -8,4 +8,12 @@ export rvm_path="/home/pi/.rvm"
 export LANG="en_US.UTF-8"
 source "/home/pi/.rvm/scripts/rvm"
 rvm use ruby-1.9.2-head
-exec "/home/pi/.rvm/gems/ruby-1.9.2-head/bin/nanoc" watch
+
+case $1 in
+  "watch"|"view")
+    action="$*" ;;
+  *)
+    echo >&2 ":: You must specify an action (view, watch)"; exit 1 ;;
+esac
+
+exec "/home/pi/.rvm/gems/ruby-1.9.2-head/bin/nanoc" $action
