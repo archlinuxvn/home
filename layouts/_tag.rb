3fb8d4eb6ec41b4dc9b3ba18bcdfd8c4a4f112b8
icy <kyanh@viettug.org>
2012-08-13 13:52:30 +0700

    layouts/_tag: add more details to reduce noise

--- layouts/_tag.rb
+++ layouts/_tag.rb
@@ -2,6 +2,6 @@
 
 <ol>
 <% items_with_tag(@tag).each do |p| %>
-  <li><%= link_to(p[:title], p.identifier) %></li>
+  <li><%= link_to(p[:title], p.identifier) %> (<%= p.identifier %>)</li>
 <% end %>
 </ol>
