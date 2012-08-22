6c6b2ceb6a60fee58b00740f53db2101d37ce2ed
icy <kyanh@viettug.org>
2012-08-22 18:26:35 +0700

    *: Clean up title

--- content/tags.rb
+++ content/tags.rb
@@ -3,8 +3,6 @@ title: Thẻ (tag) đánh dấu bài viết
 virtual: true
 ---
 
-# Thẻ (tag) đánh dấu bài viết
-
 <ul>
 <% all_tags.each do |tag, count| %>
   <li><%= link_for_tag(tag, "/tags/") %> (<%= count %> bài)</li>
