57e49999712b50981d9fd46d56022e01c480c358
icy <kyanh@viettug.org>
2012-08-22 09:26:15 +0700

    Merge with master

--- bin/watch.sh
+++ bin/watch.sh
@@ -7,5 +7,13 @@
 export rvm_path="/home/pi/.rvm"
 export LANG="en_US.UTF-8"
 source "/home/pi/.rvm/scripts/rvm"
-rvm use ruby-1.9.2-head
-exec "/home/pi/.rvm/gems/ruby-1.9.2-head/bin/nanoc" watch
+rvm use default
+
+case $1 in
+  "watch"|"view")
+    action="$*" ;;
+  *)
+    echo >&2 ":: You must specify an action (view, watch)"; exit 1 ;;
+esac
+
+exec "/home/pi/.rvm/gems/ruby-1.9.3-p194/bin/nanoc" $action
